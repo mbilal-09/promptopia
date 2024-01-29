bf3be450e54c99998179a7ceff46b1f8598cf7e3
M Bilal <121584126+mbilal-09@users.noreply.github.com>
2024-01-30 03:46:42 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ const Home = () => {
       </h1>
       <p className="desc text-center">
         Promptopia is an open-source AI prompting tool for modern world to
-        discover, create and share creative prompts
+        discover, create and share creative prompts.
       </p>
 
       <Feed />
